c92cd51147dbe5e212754505ed32c26c3db28242
VishruthRG <vishruth1020@gmail.com>
2022-11-13 22:15:10 -0600

    added mac reqs and preprocess frames

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import matplotlib.pyplot as plt
 import keras
 import tensorflow as tf
 import random
-
+import cv2
 
 env = gym.make('Pong-v4', render_mode = "human")
 
 init_frame = env.reset()
-# plt.imshow(init_frame)
 
-for i in range(10000):
+def process_frame(frame):
+    frame = frame[30:-12,5:-4]
+    frame = np.average(frame,axis = 2)
+    frame = cv2.resize(frame,(84,84),interpolation = cv2.INTER_NEAREST)
+    frame = np.array(frame,dtype = np.uint8)
+    return frame
+
+for i in range(1):
     a = random.sample([0,1,2,3], 1)[0]
-    ns, r, d, _, info = env.step(a)
-    env.render()
-    if d == True:
+    obs, rw, done, _, info = env.step(a)
+    obs = process_frame(obs)
+    plt.imshow(obs)
+    plt.show()
+    # env.render()
+    if done == True:
         env.reset()
